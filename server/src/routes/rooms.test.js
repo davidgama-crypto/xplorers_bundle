54b5d73688f38b52d6066bb24320fdeb7678513b
Shin yoon <shinmyung0@gmail.com>
2020-07-13 14:46:31 -0700

    Found a way to get the server address

--- server/src/routes/rooms.test.js
+++ server/src/routes/rooms.test.js
@@ -3,33 +3,42 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/constants');
 const app = require('../app');
 
-const request = supertest(app);
-let createdRoomId;
-let createdPlayerId1;
-let createdPlayer1Token;
-let createdPlayerId2;
-let createdPlayer2Token;
-const validNonPlayerToken = jwt.sign({ id: 'nonexistant' }, JWT_SECRET);
-
-async function finishOneGamePhase() {
-  let res = await request
-    .put(`/api/rooms/${createdRoomId}/players/${createdPlayerId1}`)
-    .set('Authorization', `Bearer ${createdPlayer1Token}`)
-    .send({
-      done: true,
-    });
-  expect(res.status).toBe(200);
-
-  res = await request
-    .put(`/api/rooms/${createdRoomId}/players/${createdPlayerId2}`)
-    .set('Authorization', `Bearer ${createdPlayer2Token}`)
-    .send({
-      done: true,
-    });
-  expect(res.status).toBe(200);
-}
-
 describe('rooms API', () => {
+  let server;
+  let request;
+  let serverAddress;
+
+  let createdRoomId;
+  let createdPlayerId1;
+  let createdPlayer1Token;
+  let createdPlayerId2;
+  let createdPlayer2Token;
+  const validNonPlayerToken = jwt.sign({ id: 'nonexistant' }, JWT_SECRET);
+
+  beforeAll(() => {
+    server = app.listen();
+    request = supertest(server);
+    serverAddress = `http://localhost:${server.address().port}`;
+  });
+
+  async function finishOneGamePhase() {
+    let res = await request
+      .put(`/api/rooms/${createdRoomId}/players/${createdPlayerId1}`)
+      .set('Authorization', `Bearer ${createdPlayer1Token}`)
+      .send({
+        done: true,
+      });
+    expect(res.status).toBe(200);
+
+    res = await request
+      .put(`/api/rooms/${createdRoomId}/players/${createdPlayerId2}`)
+      .set('Authorization', `Bearer ${createdPlayer2Token}`)
+      .send({
+        done: true,
+      });
+    expect(res.status).toBe(200);
+  }
+
   it('can create a room which returns a roomId and url', (done) => {
     request
       .get('/api/rooms')
@@ -370,5 +379,9 @@ describe('rooms API', () => {
     expect(res.body.current.round).toBe(0);
     expect(res.body.current.phase).toBe(0);
     expect(res.body.current.status).toBe('finished');
+  });
+
+  afterAll(() => {
+    server.close();
   });
 });
