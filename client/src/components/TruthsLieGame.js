0be6314ad747cbb37bcd80d763ca6dbc22c57306
davidgama-crypto <59715223+davidgama-crypto@users.noreply.github.com>
2020-07-22 21:33:36 -0700

    adapting props for Instructions page

--- client/src/components/TruthsLieGame.js
+++ client/src/components/TruthsLieGame.js
@@ -16,7 +16,9 @@ const TruthsLieGame = (props) => {
         return (
           <GameInstructions
             gameTitle="2 Truths 1 Lie"
-            gameInstructions="..................."
+            gameInstructions={['You will have 1 minute to type 2 things about you that are true and 1 thing that is a lie.',
+              'You will then submit it and after 1 minute, you will be presented with each player’s answers.',
+              'You will have 2 minutes to guess all the lies from each of the players.']}
             time={props.roomState.current.phaseDuration}
             endTime={endTimeFunction}
           />
--- client/src/utils/GamesCatalog.js
+++ client/src/utils/GamesCatalog.js
@@ -14,9 +14,8 @@ export const GAMES_CATALOG = [
   {
     id: 'TRUTHS_LIES',
     name: '2 Truths 1 Lie',
-    instructions: [{ step: 1, instruction: '1) You will have 1 minute to type 2 things about you that are true and 1 thing that is a lie.' },
-      { step: 2, instruction: '2) You will then submit it and after 1 minute, you will be presented with each player’s answers.' },
-      { step: 3, instruction: '3) You will have 2 minutes to guess all the lies from each of the players.' },
-    ],
+    instructions: ['You will have 1 minute to type 2 things about you that are true and 1 thing that is a lie.',
+      'You will then submit it and after 1 minute, you will be presented with each player’s answers.',
+      'You will have 2 minutes to guess all the lies from each of the players.'],
   },
 ];
